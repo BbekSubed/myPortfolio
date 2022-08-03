d889cbf1175d8368b838a64b94ca0ced94a91783
unknown <bibeksubedi834@gmail.com>
2022-08-03 13:29:51 +0545

    Updates

---
+++ precache-manifest.151a93718c5c8201e2bd5265da6a2051.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "280fde356257c974df67",
+    "url": "/Love/static/css/main.7081d82b.chunk.css"
+  },
+  {
+    "revision": "280fde356257c974df67",
+    "url": "/Love/static/js/main.280fde35.chunk.js"
+  },
+  {
+    "revision": "d1644c4995c369fbf0dc",
+    "url": "/Love/static/css/1.e9f22c71.chunk.css"
+  },
+  {
+    "revision": "d1644c4995c369fbf0dc",
+    "url": "/Love/static/js/1.d1644c49.chunk.js"
+  },
+  {
+    "revision": "d554d4022cd53b657101",
+    "url": "/Love/static/js/runtime~main.d554d402.js"
+  },
+  {
+    "revision": "8d278ea3fddd6336d27842e3e44d51cf",
+    "url": "/Love/static/media/cv.8d278ea3.pdf"
+  },
+  {
+    "revision": "4c4b6e154ce73d4f29c7e374b4d4e421",
+    "url": "/Love/static/media/me.4c4b6e15.jpg"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/Love/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/Love/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/Love/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/Love/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/Love/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/Love/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "228dd5fb29f9bc779c2c9309486d5a47",
+    "url": "/Love/static/media/me-about.228dd5fb.jpg"
+  },
+  {
+    "revision": "32373354fc14182bee92d4c76349b63a",
+    "url": "/Love/index.html"
+  }
+];
